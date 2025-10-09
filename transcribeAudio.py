a05b51461038586c3cd569cbf55072672766af8e
mwklincewicz <62137998+mwklincewicz@users.noreply.github.com>
2025-10-09 10:14:02 +0200

    audio to word timestamps

--- transcribeAudio.py
+++ transcribeAudio.py
@@ -3,17 +3,25 @@ import whisper_timestamped as wt
 import pandas as pd
 
 stories = ['tunnel','21styear','bronx','pieman','piemanpni']
+#stories = ["bronx"]
 
 m = whisper.load_model("large-v3")
+transcript_text = ""
 
 for task in stories:
     print( "Speech to text from: ./audio/" + task + "_audio.wav")
     result = wt.transcribe_timestamped(m, "audio/"+task+"_audio.wav", language="en", vad=True, verbose=True, temperature=0, condition_on_previous_text=False)
-    df = pd.DataFrame(columns=["phrase","word","start", "end"])
-
+    df_words = pd.DataFrame(columns=["phrase","word","start", "end"])
+    df_phrases = pd.DataFrame(columns=["text","start", "end"])
     phraseNumber = 0
     for segment in result['segments']:
+        print( segment )
         phraseNumber = phraseNumber + 1
+        df_phrases.loc[phraseNumber] = segment
         for word in segment['words']:
-            df.loc[len(df)] = [phraseNumber, word['text'], word['start'], word['end']]
-    df.to_csv("./word_timestamps/"+task+"_transcription.csv", index=False)
+            df_words.loc[len(df_words)] = [phraseNumber, word['text'], word['start'], word['end']]
+            transcript_text = transcript_text + " " + word['text']
+    df_words.to_csv("./word_timestamps/"+task+"_transcription_per_word.csv", index=False)
+    df_phrases.to_csv("./word_timestamps/"+task+"_transcription_per_phrase.csv", index=False)
+    with open("./word_timestamps/"+task+"_transcription.txt", "w+") as fh:
+        fh.write(transcript_text)
