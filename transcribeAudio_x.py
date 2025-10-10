43c7bd38452a2f12163e24b5fbd12a4173ed8e69
mwklincewicz <62137998+mwklincewicz@users.noreply.github.com>
2025-10-10 13:44:56 +0200

    audio to word timestamps, with whisperx

---
+++ transcribeAudio_x.py
@@ -0,0 +1,47 @@
+import whisperx
+import gc
+import os
+import pandas as pd
+
+stories = ['tunnel','21styear','bronx','pieman','piemanpni']
+#stories = ['bronx']
+device = "cpu"
+batch_size = 16 # reduce if low on GPU mem
+compute_type = "int8" # change to "int8" if low on GPU mem (may reduce accuracy)
+model = whisperx.load_model("large-v2", device, compute_type=compute_type)
+transcript_text = ""
+
+for task in stories:
+    audio_file = ".\\audio\\" + task + "_audio.wav"
+    if os.path.exists(audio_file):
+        print("The audio file " + audio_file + " exists.")
+    else:
+        print("The audio file " + audio_file + " DOES NOT EXIST.")
+    print( "Speech to text from: ./audio/" + task + "_audio.wav")
+    audio = whisperx.load_audio(audio_file)
+    result = model.transcribe(audio, language="en", batch_size=batch_size)
+    # print(result["segments"]) # before alignment
+
+    # 2. Align whisper output
+    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+    result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+
+    df_words = pd.DataFrame(columns=["phrase","word","start", "end"])
+    df_phrases = pd.DataFrame(columns=["text","start", "end"])
+    phraseNumber = 0
+    for segment in result['segments']:
+        print( segment )
+        phraseNumber = phraseNumber + 1
+        df_phrases.loc[phraseNumber] = segment
+        for word in segment['words']:
+            df_words.loc[len(df_words)] = [phraseNumber, word['word'], word['start'], word['end']]
+            transcript_text = transcript_text + " " + word['word']
+    df_words.to_csv("./word_timestamps/"+task+"_transcription_per_word_x.csv", index=False)
+    df_phrases.to_csv("./word_timestamps/"+task+"_transcription_per_phrase_x.csv", index=False)
+    with open("./word_timestamps/"+task+"_transcription_x.txt", "w+") as fh:
+        fh.write(transcript_text)
+
+
+
+
+import gc; import torch; gc.collect(); torch.cuda.empty_cache(); del model_a
