324dccadf3c3843f4725c2ba2190a0f2e532ee22
mwklincewicz <62137998+mwklincewicz@users.noreply.github.com>
2025-10-17 15:33:47 +0200

    beta 0.1

--- execute.py
+++ execute.py
@@ -1,39 +1,39 @@
 # import downloadWithDatalad
 # #import deleteWithDatalad
-# import firstLevelModel_virtues
-# import firstLevelModel_vices
+import firstLevelModel_virtues
+import firstLevelModel_vices
 import secondLevelModel_1vB_MAC_virtues
 import secondLevelModel_1v6_MAC_virtues
 import secondLevelModel_1vB_MAC_vices
 import secondLevelModel_1v6_MAC_vices
-# import secondLevelModel_virtues
-# import secondLevelModel_vices
+import secondLevelModel_virtues
+import secondLevelModel_vices
 # import transcribeAudio_x
 
 #stories = ['shapessocial','shapesphysical','21styear','bronx','pieman','piemanpni','tunnel']
-# stories = ['pieman', 'piemanpni']
+stories = ['pieman']
 #task = "pieman"
 
-# for task in stories:
-#     # transcribeAudio_x.run(task)
-#     #
-#     # downloadWithDatalad.run(task)
-#     firstLevelModel_virtues.run(task)
-#     firstLevelModel_vices.run(task)
-#     secondLevelModel_virtues.run(task)
-#     secondLevelModel_vices.run(task)
-#
-#
-#     for foundation in range(1,8):
-#         secondLevelModel_1v6_MAC_virtues.run(task, foundation)
-#         secondLevelModel_1vB_MAC_virtues.run(task, foundation)
-#         secondLevelModel_1v6_MAC_vices.run(task, foundation)
-#         secondLevelModel_1vB_MAC_vices.run(task, foundation)
+for task in stories:
+    # transcribeAudio_x.run(task)
+    #
+    # downloadWithDatalad.run(task)
+    firstLevelModel_virtues.run(task)
+    firstLevelModel_vices.run(task)
+    secondLevelModel_virtues.run(task)
+    secondLevelModel_vices.run(task)
+
+
+    for foundation in range(1,8):
+        secondLevelModel_1v6_MAC_virtues.run(task, foundation)
+        secondLevelModel_1vB_MAC_virtues.run(task, foundation)
+        secondLevelModel_1v6_MAC_vices.run(task, foundation)
+        secondLevelModel_1vB_MAC_vices.run(task, foundation)
 
     #deleteWithDatalad.run(task)
 
-for foundation in range(1,8):
-    secondLevelModel_1v6_MAC_virtues.run("bronx", foundation)
-    secondLevelModel_1vB_MAC_virtues.run("bronx", foundation)
-    secondLevelModel_1v6_MAC_vices.run("bronx", foundation)
-    secondLevelModel_1vB_MAC_vices.run("bronx", foundation)
+# for foundation in range(1,8):
+#     secondLevelModel_1v6_MAC_virtues.run("bronx", foundation)
+#     secondLevelModel_1vB_MAC_virtues.run("bronx", foundation)
+#     secondLevelModel_1v6_MAC_vices.run("bronx", foundation)
+#     secondLevelModel_1vB_MAC_vices.run("bronx", foundation)
